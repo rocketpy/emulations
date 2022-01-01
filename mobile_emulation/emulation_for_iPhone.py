c5f2143486e3186d1ccab2782b2cad3da9ae2e51
rocketpy <dimitr.ushakov@gmail.com>
2022-01-01 19:41:48 +0200

    Update emulation_for_iPhone.py

--- mobile_emulation/emulation_for_iPhone.py
+++ mobile_emulation/emulation_for_iPhone.py
@@ -1,1 +1,9 @@
+from selenium import webdriver
 
+
+mob = { "deviceName": "your device" }
+chrome_options = webdriver.ChromeOptions()
+chrome_options.add_experimental_option("mobileEmulation", mob)
+driver = webdriver.Chrome(options=chrome_options)  # execution path
+
+driver.get('https://www...')
