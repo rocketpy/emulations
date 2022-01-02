60342aea4ca6ae5151ec36a0ea212c25c1fbf5c7
rocketpy <dimitr.ushakov@gmail.com>
2022-01-02 23:07:58 +0200

    Update emulation_for_iPhone.py

--- mobile_emulation/emulation_for_iPhone.py
+++ mobile_emulation/emulation_for_iPhone.py
@@ -18,4 +18,9 @@ from selenium.webdriver.chrome.options import Options
 mob = {"deviceMetrics": { "width": 375, "height": 812, "pixelRatio": 3.0 },
        "userAgent": "Mozilla/5.0 (Linux; Android 4.2.1; en-us; Nexus 5 Build/JOP40D) AppleWebKit/535.19 (KHTML, like Gecko) Chrome/18.0.1025.166 Mobile Safari/535.19"
       }
+chrome_options = Options()
+chrome_options.add_experimental_option("mobileEmulation", mobile_emulation)
+driver = webdriver.Chrome(executable_path="../chrome/chromedriver85", options=chrome_options)  # execution path
+url = "https://www.../"
+driver.get(url)
 
