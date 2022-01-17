72805cdf18e257cbf1867cf02cd1c6ab9ff4ebf4
rocketpy <dimitr.ushakov@gmail.com>
2022-01-17 23:14:12 +0200

    Update Emulation_Playwright.py

--- mobile_emulation/Emulation_Playwright.py
+++ mobile_emulation/Emulation_Playwright.py
@@ -1,1 +1,17 @@
+# Playwright - can be used to simulate browser behavior on a mobile device
 
+# https://playwright.dev/python/docs/emulation
+
+# 
+from playwright.sync_api import sync_playwright
+
+
+def run(playwright):
+    pixel_2 = playwright.devices['Pixel 2']
+    browser = playwright.webkit.launch(headless=False)
+    context = browser.new_context(
+        **pixel_2,
+    )
+
+with sync_playwright() as playwright:
+    run(playwright)
